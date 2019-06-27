0e60ffec530c8376f4e7450dcac013a37b7801f3
Tammivuori <marko.tammivuori@gmail.com>
2019-06-27 18:26:58 +0300

    fllstack 1.9: unicafe step4 ready

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,12 +1,28 @@
-//MarkoT fllstack 1.8: unicafe step3
+//MarkoT fllstack 1.9: unicafe step4
 
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistis = (props) => (
-    <p>{props.text} {props.nrb} {props.pros}</p>
-)
+// Component to create statistics
+const Statistic = (props) => {
+  if (props.allNbr === 0) {
+    return (
+      <p>No feedback has been given sofar. The app is used by pressing the buttons.</p> 
+    )
+  }
+  return (
+    <div>
+      <p>{props.goodTxt} {props.goodNbr}</p>
+      <p>{props.neutralTxt} {props.neutralNbr}</p>
+      <p>{props.badTxt} {props.badNbr}</p>
+      <p>{props.allTxt} {props.allNbr}</p>
+      <p>{props.averageTxt} {props.averageNbr}</p>
+      <p>{props.positiveTxt} {props.positiveNbr} {props.positivePros}</p>
+    </div>
+    )
+}
 
+// Component to create all buttons and eventlistener
 const Button = (props) => {
     // console.log(props)
     const { handleOnClick, text } = props
@@ -33,22 +49,23 @@ const App = () => {
 
   return (
     <div>
-        <p>MarkoT fllstack 1.8: unicafe step3</p>
-        <h2>Unicafe - give feedback</h2>
+        <p>MarkoT fllstack 1.9: unicafe step4</p>
 
+        <h2>Unicafe - give feedback</h2>
         <Button handleOnClick={() => increaseGood(good+1)} text='good'/>
         <Button handleOnClick={() => increaseNeutral(neutral+1)} text='neutral'/>
         <Button handleOnClick={() => increaseBad(bad+1)} text='bad'/>
 
 
-        <h3>statistics</h3>
-        <Statistis text="good: " nrb={good}/>
-        <Statistis text="neutral: " nrb={neutral}/>
-        <Statistis text="bad: " nrb={bad}/>
-        <Statistis text="all: " nrb={totalNumber}/>
-        <Statistis text="average: " nrb={(( good - bad ) / totalNumber )}/>      
-        <Statistis text="positive: " nrb={(( good / totalNumber ) * 100 )} pros=" %"/>      
-
+        <h4>Statistics</h4>
+        <Statistic
+          goodTxt="good: " goodNbr={good}
+          neutralTxt="neutral: " neutralNbr={neutral}
+          badTxt="bad: " badNbr={bad}
+          allTxt="all: " allNbr={totalNumber}
+          averageTxt="average: " averageNbr={(( good - bad ) / totalNumber )}
+          positiveTxt="positive: " positiveNbr={(( good / totalNumber ) * 100 )} positivePros=" %"
+          />
     </div>
   )
 }
